024983189309b0f0dc05bdc6f2335f37d6fc5d13
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2025-02-13 23:54:36 +0700

    added gitignore

---
+++ cmake-build-debug/CMakeFiles/ImageCpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageCpp.dir/main.cpp.o"
+  "CMakeFiles/ImageCpp.dir/main.cpp.o.d"
+  "ImageCpp"
+  "ImageCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
